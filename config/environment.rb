1e0bf2143c57c322328390e07dcd1196c139f84a
Wendy Calderon <wendycalderon@gmail.com>
2019-08-29 17:23:34 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,8 @@
 require 'bundler/setup'
+require 'app'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
-
-require_all 'app'
